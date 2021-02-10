5705ca3802487747e1ba5c094e40c56b79031a08
Zachary Lynch <zachary.lynch1337@gmail.com>
2021-02-10 19:30:17 +0000

    Automatically backed up by Learn

--- js/createStore.js
+++ js/createStore.js
@@ -15,13 +15,24 @@ function createStore() {
     getState
   };
 };
-
-
+ 
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+ 
+    default:
+      return state;
+  }
+};
+ 
 function render() {
   let container = document.getElementById('container');
   container.textContent = store.getState().count;
 };
-
+ 
+let store = createStore();
+store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
  
 button.addEventListener('click', () => {
